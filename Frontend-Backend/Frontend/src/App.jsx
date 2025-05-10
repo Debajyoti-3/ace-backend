d494b3ea89529e15eb2641bf70102d036af7eecb
Debajyoti Mitra <debajyotimitra369@gmail.com>
2025-05-11 01:47:22 +0530

    commited

--- Frontend-Backend/Frontend/src/App.jsx
+++ Frontend-Backend/Frontend/src/App.jsx
@@ -5,26 +5,27 @@ import axios from "axios";
 function App() {
   const [jokes, setJokes] = useState([]);
 
-  console.log(`hello devs`)
+  console.log(`hello devs`);
 
-  useEffect(()=>{
-    axios.get(`/api/jokes`)
-    .then((response) => {
-    console.log(`response data ${response.data}`)
-      setJokes(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  },[])
-  
+  useEffect(() => {
+    console.log(`Debajyoti`); // its confirming that useEffect is triggerd
+
+    axios
+      .get(`/api/jokes`)
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   return (
     <>
       <h1>Learning Full Stack</h1>
       <h2>JOKES: {jokes.length}</h2>
 
-      {jokes.map((joke,index) => (
+      {jokes.map((joke, index) => (
         <div key={joke.id}>
           <h2>{joke.title}</h2>
           <p>{joke.content}</p>
--- Frontend-Backend/Frontend/vite.config.js
+++ Frontend-Backend/Frontend/vite.config.js
@@ -5,11 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-      },
+      "/api": "http://localhost:4000",
     },
   },
+
   plugins: [react()],
 });
