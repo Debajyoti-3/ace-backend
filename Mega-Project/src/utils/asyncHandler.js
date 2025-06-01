fa8b86f2422862a9da557dcd4e212220ec387db7
Debajyoti Mitra <debajyotimitra369@gmail.com>
2025-06-01 11:47:33 +0530

    feat: adding files in utils

---
+++ Mega-Project/src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+/* here asyncHandler is a high order function (its a function who can takes functions
+as arguments and can return functions)
+*/
+
+// using promise (may seems complex)
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+
+// const asyncHandler = ()=>{}
+// const asyncHandler = ()=>{() => {}}
+// const asyncHandler = ()=>{ async () => {}}
+
+        // using try-catch (easy way)
+// const asyncHandler = (fn)=> async (res,req,next)=>{
+//     try {
+//         await fn(res,req,next)
+//     } catch (error) {
+//         res.status( error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
