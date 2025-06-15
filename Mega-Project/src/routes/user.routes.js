833958e68dd1191f25b48df1edb202d77a0189a8
Debajyoti Mitra <debajyotimitra369@gmail.com>
2025-06-16 04:53:39 +0530

    feat: adding updates in router

--- Mega-Project/src/routes/user.routes.js
+++ Mega-Project/src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changePassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateCoverImage, 
+    getWatchHistory 
+} 
+from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 const router = Router()
 
@@ -22,8 +34,19 @@ router.route("/login").post(loginUser)
 
 
 // secured routes (means here user have to logged in)
+
 router.route("/logout").post(verifyJWT, logoutUser)     // verifyJWT -> middleware
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changePassword)
+router.route("/current-user").get(getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 
 
