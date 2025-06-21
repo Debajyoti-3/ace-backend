e66da57680592b70276b2f78055f1c7cc70ae791
Debajyoti Mitra <debajyotimitra369@gmail.com>
2025-06-21 21:01:20 +0530

    fix: fix addTweet and getUserTweets logic

--- Mega-Project/src/controllers/tweet.controller.js
+++ Mega-Project/src/controllers/tweet.controller.js
@@ -9,27 +9,58 @@ const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
 
     const {content} = req.body;
-    if(!content){
-        throw new ApiError(400,"Tweet not Exist")
+    const {tweetId}= req.params
+    const {userId} = req.user
+
+    if(!content || !tweetId){
+        throw new ApiError(400,"Required fields are not given")
     }
+
+    if(!userId){
+        throw new ApiError(400, "userId not found")
+    }
+
+    const addTweet = await Tweet.create({
+        content:content,
+        owner:userId
+    })
 
     return res
     .status(200)
-    .json(new ApiResponse(200,content,"Tweet Created successfully"))
+    .json(new ApiResponse(200,addTweet,"Tweet Created successfully"))
     
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
     
-    const tweet = await Tweet.findById(req.params?._id)
-    if(!tweet){
-        throw new ApiError(400,"tweet not found");
-    }
+    // const tweet = await Tweet.findById(req.params?._id)
+    // if(!tweet){
+    //     throw new ApiError(400,"tweet not found");
+    // }
+    const {userId} = req.user
+    const result = await User.aggregate([
+        {
+            $match:{    // filtering
+                _id:userId,
+            }
+        },
+        {
+            $lookup:{   // join
+                from:"tweets",
+                localField:"_id",
+                foreignField:"owner",
+                as:"userTweets"
+            }
+        },
+        {
+            $unwind: "$userTweets"
+        }
+    ])
 
     return res
     .status(200)
-    .json(new ApiResponse (200,tweet,"tweet fetched successfully"))
+    .json(new ApiResponse (200,result,"user's tweets  fetched successfully"))
 
 })
 
